4699c7bed14b48c77b6d08fcbc0bf5d0b3c38f26
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-08-05 08:24:28 +0530

    Set Note model instance name

--- backend/noteopia/models.py
+++ backend/noteopia/models.py
@@ -6,3 +6,6 @@ class Note(models.Model):
     title = models.CharField(max_length=100, unique=True)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.title}'
