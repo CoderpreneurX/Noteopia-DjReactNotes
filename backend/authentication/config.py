32a0664a2633115465493f15a69622e1f0da277f
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-08-07 01:25:09 +0530

    Moved settings to authentication.config

--- backend/authentication/config.py
+++ backend/authentication/config.py
@@ -1,12 +1,22 @@
 from .apps import AuthenticationConfig
+from datetime import timedelta
 
 app_name = AuthenticationConfig.name
 
 def apply_configurations(globals_dict):
     installed_apps = globals_dict["INSTALLED_APPS"]
     installed_apps.append(app_name)
+    installed_apps.append('rest_framework')
+    installed_apps.append('corsheaders')
     globals_dict["REST_FRAMEWORK"] = {
         "DEFAULT_AUTHENTICATION_CLASSES": (
             "rest_framework_simplejwt.authentication.JWTAuthentication",
         )
     }
+    globals_dict["SIMPLE_JWT"] = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=1)
+    }
+    globals_dict["CORS_ALLOW_ALL_ORIGINS"] = True
+    globals_dict["CORS_ALLOW_CREDENTIALS"] = True
+    globals_dict["MIDDLEWARE"].append("corsheaders.middleware.CorsMiddleware")
